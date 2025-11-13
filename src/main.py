5fb27af02d40ff3f61ddfed030d911e69d7f98b0
Michal <michal.zaczek@interia.pl>
2025-11-13 16:59:59 +0100

    feat: create utilities/copy_files function to recursively copy files and folders

--- src/main.py
+++ src/main.py
@@ -1,8 +1,9 @@
-from utilities import delete_from_folder
+from utilities import copy_files, delete_from_folder
 
 
 def main():
     delete_from_folder("public")
+    copy_files("static", "public")
 
 
 if __name__ == "__main__":
--- src/utilities.py
+++ src/utilities.py
@@ -8,3 +8,18 @@ def delete_from_folder(folder_path):
         shutil.rmtree(folder_path)
     # Recreate the public directory
     os.makedirs(folder_path)
+
+
+def copy_files(from_path, to_path):
+    dir = os.listdir(from_path)
+
+    if not os.path.exists(to_path):
+        os.mkdir(to_path)
+
+    for item in dir:
+        item_path = os.path.join(from_path, item)
+        if os.path.isfile(item_path):
+            shutil.copy(item_path, to_path)
+        else:
+            new_to_path = os.path.join(to_path, item)
+            copy_files(item_path, new_to_path)
