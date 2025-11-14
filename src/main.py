ea786a6de51478401dafb523878ed8c4b3dd33cc
Michal <michal.zaczek@interia.pl>
2025-11-14 15:32:14 +0100

    feat: integrate generate_page into main build process and add dev server

--- src/main.py
+++ src/main.py
@@ -1,9 +1,10 @@
-from utilities import copy_files, delete_from_folder
+from utilities import copy_files, delete_from_folder, generate_page
 
 
 def main():
     delete_from_folder("public")
     copy_files("static", "public")
+    generate_page("content/index.md", "template.html", "public/index.html")
 
 
 if __name__ == "__main__":
