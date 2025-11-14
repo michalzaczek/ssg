65f7a66980196ef568f2e71f55013fde6ec2dc9d
Michal <michal.zaczek@interia.pl>
2025-11-14 15:27:58 +0100

    feat: create utilities/generate_page function to convert md to html file

--- src/utilities.py
+++ src/utilities.py
@@ -1,6 +1,8 @@
 import os
 import shutil
 
+from textnode import markdown_to_html_node
+
 
 def delete_from_folder(folder_path):
     # Delete the public directory if it exists
@@ -10,20 +12,20 @@ def delete_from_folder(folder_path):
     os.makedirs(folder_path)
 
 
-def copy_files(from_path, to_path):
+def copy_files(from_path, dest_path):
     dir = os.listdir(from_path)
 
-    if not os.path.exists(to_path):
-        os.mkdir(to_path)
+    if not os.path.exists(dest_path):
+        os.mkdir(dest_path)
 
     for item in dir:
         item_path = os.path.join(from_path, item)
         if os.path.isfile(item_path):
-            print(f"Copying file: {item_path} -> {to_path}")
-            shutil.copy(item_path, to_path)
+            print(f"Copying file: {item_path} -> {dest_path}")
+            shutil.copy(item_path, dest_path)
         else:
-            new_to_path = os.path.join(to_path, item)
-            copy_files(item_path, new_to_path)
+            new_dest_path = os.path.join(dest_path, item)
+            copy_files(item_path, new_dest_path)
 
 
 def extract_title(markdown):
@@ -39,3 +41,29 @@ def extract_title(markdown):
             title = stripped[1:].strip()
             return title
     raise Exception("Error: No h1 header found in markdown")
+
+
+def generate_page(from_path, template_path, dest_path):
+    """
+    Generate an HTML page from a markdown file using a template.
+    """
+    print(f"Generating page from {from_path} to {dest_path} using {template_path}")
+    with open(from_path, "r") as file:
+        md = file.read()
+
+    with open(template_path, "r") as file:
+        template = file.read()
+
+    html_node = markdown_to_html_node(md)
+    html_str = html_node.to_html()
+    title = extract_title(md)
+
+    new_html = template.replace("{{ Title }}", title).replace("{{ Content }}", html_str)
+
+    # Create necessary directories if they don't exist
+    dest_dir = os.path.dirname(dest_path)
+    if dest_dir:
+        os.makedirs(dest_dir, exist_ok=True)
+
+    with open(dest_path, "w") as file:
+        file.write(new_html)
